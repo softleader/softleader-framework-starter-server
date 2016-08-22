8d23249856de143c15bfbe70475a42dd7f6ae7a0
Matt S.Y. Ho <methodho@gmail.com>
2016-08-22 11:16:20 +0800

    switch from c3p0 to bonecp

--- src/main/java/tw/com/softleader/starter/server/config/DataSourceConfig.java
+++ src/main/java/tw/com/softleader/starter/server/config/DataSourceConfig.java
@@ -2,13 +2,18 @@ package tw.com.softleader.starter.server.config;
 
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.jolbox.bonecp.BoneCPDataSource;
+
 import tw.com.softleader.commons.collect.Lists;
 import tw.com.softleader.data.config.DataSourceConfiguration;
+import tw.com.softleader.data.config.DataSourceDefinitions;
+import tw.com.softleader.data.config.DatabaseDefinition;
 import tw.com.softleader.data.dao.GenericCrudDaoImpl;
 import tw.com.softleader.data.entity.EntityPersistenceCallbackSupplier;
 import tw.com.softleader.security.supplier.CurrentUsernameSupplier;
@@ -35,5 +40,18 @@ public class DataSourceConfig extends DataSourceConfiguration {
   @Bean
   public CurrentUsernameSupplier currentUsernameSupplier() {
     return new CurrentUsernameSupplier();
+  }
+  
+  @Bean
+  public DataSourceDefinitions dataSourceDefinitions() {
+    BoneCPDataSource ds = new BoneCPDataSource();
+    ds.setDriverClass(DATASOURCE_PROPS.getProperty(DatabaseDefinition.DRIVER_CLASS));
+    ds.setJdbcUrl(DATASOURCE_PROPS.getProperty(DatabaseDefinition.URL));
+    ds.setUsername(DATASOURCE_PROPS.getProperty(DatabaseDefinition.USERNAME));
+    ds.setPassword(DATASOURCE_PROPS.getProperty(DatabaseDefinition.PASSWORD));
+
+    DataSourceDefinitions definitions = new DataSourceDefinitions();
+    definitions.setTargetDataSource(DataSourceDefinitions.TYPE.DEFAULT.name(), ds);
+    return definitions;
   }
 }
