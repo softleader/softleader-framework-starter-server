c3824b14a8861aa893f8826b1cea5bbb5f366e68
Matt S.Y. Ho <methodho@gmail.com>
2016-08-22 13:26:43 +0800

    update

--- src/main/java/tw/com/softleader/starter/server/config/DataSourceConfig.java
+++ src/main/java/tw/com/softleader/starter/server/config/DataSourceConfig.java
@@ -2,7 +2,6 @@ package tw.com.softleader.starter.server.config;
 
 import java.util.Collection;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
