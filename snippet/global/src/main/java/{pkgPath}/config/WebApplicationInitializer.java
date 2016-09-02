16bb43b47a7c408fafb4359a06a83c76512f9ddb
Matt S.Y. Ho <methodho@gmail.com>
2016-09-02 18:26:05 +0800

    change to mysql

--- snippet/global/src/main/java/{pkgPath}/config/WebApplicationInitializer.java
+++ snippet/global/src/main/java/{pkgPath}/config/WebApplicationInitializer.java
@@ -1,6 +1,8 @@
 package {pkg}.config;
 
 import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -26,5 +28,17 @@ public class WebApplicationInitializer
   protected String[] getServletMappings() {
     return new String[] {"/"};
   }
+  
+  @Override
+  protected void customizeRegistration(Dynamic registration) {
+    super.customizeRegistration(registration);
 
+    String location = ""; // the directory location where files will be stored
+    long maxFileSize = -1; // the maximum size allowed for uploaded files
+    long maxRequestSize = -1; // the maximum size allowed for multipart/form-data requests
+    int fileSizeThreshold = 0; // the size threshold after which files will be written to disk
+    registration.setMultipartConfig(
+        new MultipartConfigElement(location, maxFileSize, maxRequestSize, fileSizeThreshold));
+  }
+  
 }
--- snippet/softleader-security/src/main/java/{pkgPath}/config/SecurityWebApplicationInitializer.java
+++ snippet/softleader-security/src/main/java/{pkgPath}/config/SecurityWebApplicationInitializer.java
@@ -1,9 +1,16 @@
 package {pkg}.config;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.multipart.support.MultipartFilter;
 
 public class SecurityWebApplicationInitializer
     extends AbstractSecurityWebApplicationInitializer {
 
-
+  @Override
+  protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+    insertFilters(servletContext, new MultipartFilter());
+  }
 }
+
