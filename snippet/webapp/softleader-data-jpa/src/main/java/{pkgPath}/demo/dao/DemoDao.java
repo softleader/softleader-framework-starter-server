ecd88d065d9da1aed995e74147df362abc660d0b
Matt S.Y. Ho <methodho@gmail.com>
2016-10-11 15:35:31 +0800

    zip snippet

--- snippet/webapp/softleader-data-jpa/src/main/java/{pkgPath}/demo/dao/DemoDao.java
+++
@@ -1,8 +0,0 @@
-package {pkg}.demo.dao;
-
-import tw.com.softleader.data.dao.GenericCrudCodeDao;
-import {pkg}.demo.entity.Demo;
-
-public interface DemoDao extends GenericCrudCodeDao<Demo, Long> {
-
-}
