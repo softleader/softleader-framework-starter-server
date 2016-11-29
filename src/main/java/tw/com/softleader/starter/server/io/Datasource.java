86b14522f631da9fa1ab7528c305dbb058d0d4ad
Matt S.Y. Ho <methodho@gmail.com>
2016-11-29 16:43:36 +0800

    Update

--- src/main/java/tw/com/softleader/starter/server/io/Datasource.java
+++ src/main/java/tw/com/softleader/starter/server/io/Datasource.java
@@ -2,6 +2,11 @@ package tw.com.softleader.starter.server.io;
 
 import java.util.Optional;
 import java.util.function.UnaryOperator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.assertj.core.util.VisibleForTesting;
+import org.springframework.orm.jpa.vendor.Database;
 
 import lombok.RequiredArgsConstructor;
 import tw.com.softleader.starter.server.pojo.Snippet;
@@ -9,12 +14,14 @@ import tw.com.softleader.starter.server.pojo.Snippet;
 @RequiredArgsConstructor
 public class Datasource implements UnaryOperator<String> {
 
+  private static Pattern FIRST_WORD = Pattern.compile("^(\\w+)\\b");
+
   private final Snippet starter;
 
   @Override
   public String apply(String source) {
 
-    source = source.replace("{database}", starter.getDatabase().getName().toUpperCase());
+    source = source.replace("{database}", smartAnalyseDatabase(starter.getDatabase().getName()));
     source = source.replace("{driverClass}", starter.getDatabase().getDriverClass());
     source = source.replace("{url}", starter.getDatabase().getUrl());
     source = source.replace("{username}", starter.getDatabase().getUsername());
@@ -22,6 +29,31 @@ public class Datasource implements UnaryOperator<String> {
         Optional.ofNullable(starter.getDatabase().getPassword()).orElse(""));
 
     return source;
+  }
+
+  @VisibleForTesting
+  String smartAnalyseDatabase(String name) {
+    // special rules
+    if (name.startsWith("Microsoft sql")) {
+      return Database.SQL_SERVER.name();
+    }
+    // matches whole words
+    for (Database database : Database.values()) {
+      if (database.name().equalsIgnoreCase(name)) {
+        return database.name();
+      }
+    }
+    // matches first word
+    Matcher m = FIRST_WORD.matcher(name);
+    if (m.find()) {
+      String firstLetter = m.group(1);
+      for (Database database : Database.values()) {
+        if (database.name().equalsIgnoreCase(firstLetter)) {
+          return database.name();
+        }
+      }
+    }
+    return "Choose a database vendor from 'org.springframework.orm.jpa.vendor.Database'";
   }
 
 }
---
+++ src/test/java/tw/com/softleader/starter/server/io/DatasourceTest.java
@@ -0,0 +1,22 @@
+package tw.com.softleader.starter.server.io;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.orm.jpa.vendor.Database;
+
+
+public class DatasourceTest {
+
+  @Test
+  public void testSmartAnalyseDatabase() {
+    Datasource ds = new Datasource(null);
+
+    Assert.assertEquals(Database.POSTGRESQL.name(), ds.smartAnalyseDatabase("PostgreSQL"));
+    Assert.assertEquals(Database.MYSQL.name(), ds.smartAnalyseDatabase("MySQL"));
+    Assert.assertEquals(Database.ORACLE.name(), ds.smartAnalyseDatabase("Oracle Thin ojdbc14"));
+    Assert.assertEquals(Database.SQL_SERVER.name(), ds.smartAnalyseDatabase("Microsoft sqljdbc4"));
+    Assert.assertEquals(Database.HSQL.name(), ds.smartAnalyseDatabase("HSQL"));
+    Assert.assertEquals(Database.H2.name(), ds.smartAnalyseDatabase("H2"));
+  }
+
+}
